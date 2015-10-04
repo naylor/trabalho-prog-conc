1ea9f0b08972d83834cd6e38c1932df578a008df
Naylor Garcia <naylorgarcia@gmail.com>
2015-10-04 08:25:42 -0300

    Correcoes.

--- src/main.c
+++ src/main.c
@@ -43,7 +43,7 @@ int main (int argc, char *argv[]){
 
             switch (ct->typeAlg) {
                 case 's':
-                    printf("Sequencial\n");
+                    printf("Sequencial: %s\n", ct->filePath);
                     sequencial(m);
                     break;
                 case 'p':
